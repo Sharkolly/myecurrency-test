bcac9f076c1f63dee9e0ade74494b1203e42c110
Sharkolly <sharkollymofeoluwa@gmail.com>
2024-11-02 18:54:33 +0100

    made few changes to design

--- src/components/Shop/Intro.jsx
+++ src/components/Shop/Intro.jsx
@@ -1,6 +1,6 @@
 const Intro = () => {
   return (
-    <header className="h-[85vh] z-[-11] bg-fixed bg-no-repeat w-full bg-cover bg-center relative shop" style={{ backgroundImage: 'url("./src/assets/man.png")' }}>
+    <header className="h-[85vh] z-[-11] bg-fixed bg-no-repeat w-full bg-cover bg-center relative shop" style={{ backgroundImage: 'url(/man.png)' }}>
       <div className="pt-[11em] w-9/12 mx-auto max-lg:w-[92%] max-lg:pt-[20em] max-md:pt-[18em]">
         <div className="w-[55%] flex flex-col gap-5 max-lg:w-[92%] max-lg:mx-auto">
           <p>We&apos;re here to help you</p>
